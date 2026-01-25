68a87dbd1ea5006a6a6d4513164cc75315cec55c
Anatoly <tolikto@yandex.ru>
2026-01-25 16:43:14 +0300

    Утранено замечание с github по удалению пробела перед угловой скобкой

--- src/main/java/ru/yandex/practicum/sleeptracker/analiticalfunctions/SleepSessionsCounter.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/analiticalfunctions/SleepSessionsCounter.java
@@ -5,7 +5,7 @@ import ru.yandex.practicum.sleeptracker.SleepSession;
 import java.util.List;
 import java.util.function.Function;
 
-public class SleepSessionsCounter implements Function <List<SleepSession>, Long> {
+public class SleepSessionsCounter implements Function<List<SleepSession>, Long> {
     @Override
     public Long apply(List<SleepSession> sleepSessions) {
         return sleepSessions.stream().count();
